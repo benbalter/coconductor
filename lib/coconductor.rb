938a7d49a4d060c2d346bd243fa92bbe9f88e91c
Ben Balter <ben.balter@github.com>
2018-08-22 13:21:24 -0400

    add helpers on module

--- lib/coconductor.rb
+++ lib/coconductor.rb
@@ -8,4 +8,26 @@ module Coconductor
   autoload :ProjectFiles,  'coconductor/project_files'
 
   CONFIDENCE_THRESHOLD = 90
+
+  class << self
+    attr_writer :confidence_threshold
+
+    def codes_of_conduct
+      CodeOfConduct.all
+    end
+
+    def code_of_conduct(path)
+      Coconductor.project(path).code_of_conduct
+    end
+
+    def project(path, **args)
+      Coconductor::Projects::GitProject.new(path, args)
+    rescue Coconductor::Projects::GitProject::InvalidRepository
+      Coconductor::Projects::FSProject.new(path, args)
+    end
+
+    def confidence_threshold
+      @confidence_threshold ||= CONFIDENCE_THRESHOLD
+    end
+  end
 end
--- spec/coconductor_spec.rb
+++ spec/coconductor_spec.rb
@@ -1,5 +1,40 @@
 RSpec.describe Coconductor do
+  let(:cc_1_4) do
+    Coconductor::CodeOfConduct.find('contributor-covenant/version/1/4')
+  end
+  let(:path) { project_root }
+
   it 'has a version number' do
-    expect(Coconductor::VERSION).not_to be nil
+    expect(described_class::VERSION).not_to be nil
+  end
+
+  it 'returns all codes of conduct' do
+    expect(described_class.codes_of_conduct.count).to eql(47)
+    expect(described_class.codes_of_conduct).to all(be_a(Coconductor::CodeOfConduct))
+  end
+
+  it 'returns the code of conduct for a given path' do
+    expect(described_class.code_of_conduct(path)).to eql(cc_1_4)
+  end
+
+  it 'returns the project for a given path' do
+    project = described_class.project(path)
+    expect(project).to be_a(Coconductor::Projects::GitProject)
+    expect(project.code_of_conduct).to eql(cc_1_4)
+  end
+
+  context '#confidence_threshold' do
+    it 'returns the confidence threshold' do
+      expect(subject.confidence_threshold).to eql(90)
+    end
+
+    context 'user overridden' do
+      before { described_class.confidence_threshold = 50 }
+      after { described_class.confidence_threshold = nil }
+
+      it 'lets the user override the confidence threshold' do
+        expect(described_class.confidence_threshold).to eql(50)
+      end
+    end
   end
 end
