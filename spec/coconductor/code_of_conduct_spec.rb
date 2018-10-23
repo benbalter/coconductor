ca31ddd2b3c70cc046136843e8d6a83f9ffff924
Ben Balter <ben.balter@github.com>
2018-10-23 17:53:54 -0400

    add go coc

--- spec/coconductor/code_of_conduct_spec.rb
+++ spec/coconductor/code_of_conduct_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Coconductor::CodeOfConduct do
-  let(:code_of_conduct_count) { 53 }
+  let(:code_of_conduct_count) { 54 }
 
   context 'class methods' do
     it 'loads all codes of conduct' do
@@ -46,6 +46,7 @@ RSpec.describe Coconductor::CodeOfConduct do
         citizen-code-of-conduct
         contributor-covenant
         django
+        go
       ].sort
       expect(described_class.families.sort).to eql(expected)
     end
@@ -57,6 +58,7 @@ RSpec.describe Coconductor::CodeOfConduct do
         citizen-code-of-conduct/version/2/3
         contributor-covenant/version/1/4
         django/version/1/0
+        go/version/1/0
       ].sort
       expect(described_class.latest.map(&:key).sort).to eql(expected)
     end
--- spec/coconductor/field_spec.rb
+++ spec/coconductor/field_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe Coconductor::Field do
-  let(:all_field_count) { 11 }
+  let(:all_field_count) { 12 }
   let(:cc_field_cound) { 1 }
   let(:name) { 'SOME_FIELD' }
   let(:description) { 'Some description.' }
